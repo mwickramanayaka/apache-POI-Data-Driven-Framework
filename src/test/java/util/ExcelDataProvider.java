291374d1e1e1eb1b45ff244d753ac6235ad7d59b
mwickramanayaka <manulwick@gmail.com>
2021-06-02 21:55:41 +0530

    final commit

--- src/test/java/util/ExcelDataProvider.java
+++ src/test/java/util/ExcelDataProvider.java
@@ -1,33 +1,75 @@
 package util;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class ExcelDataProvider {
 
-	public static void main(String[] args) {
+	WebDriver driver = null;
+
+	@BeforeTest
+	public void setUpTest() {
+		//initializing and starting the browser
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+
+	}
+
+	@Test(dataProvider="test1data")
+	public void test1(String USERNAME, String PASSWORD) throws InterruptedException {
+		System.out.println(USERNAME + "|" + PASSWORD);
+		
+		driver.get("https://vle.bit.lk/login/index.php");
+		driver.findElement(By.id("username")).sendKeys(USERNAME);
+		driver.findElement(By.id("password")).sendKeys(PASSWORD);
+		Thread.sleep(2000);
+		
+	}
+	
+	@AfterTest
+	public void tearDown() {
+		driver.quit();
+		
+	}
+
+	@DataProvider(name = "test1data")
+	public static Object[][] getData() {
+
 		String projectPath = System.getProperty("user.dir");
 		String excelPath = projectPath + "/Excel Files/data1.xlsx";
 
-		testData(excelPath, "Sheet1");
-	}	
+		Object data[][] = testData(excelPath, "Sheet1");
+		return data;
 
+	}
+ 
 	public static Object[][] testData(String exelPath, String sheetName) {
 
 		ExcelUtilTest excel = new ExcelUtilTest (exelPath, sheetName);
 
 		int rowCount = excel.getRowCount();
-		int colCount = excel.getColCount();
+		int colCount = excel.getColCount();  
 
-		Object data[0][0] = new Object[rowCount-1][colCount];
+		Object data[][] = new Object[rowCount-1][colCount];
 
 		for (int i = 1; i < rowCount; i++) {
 			for(int j = 0; j < colCount; j++) {
 				String cellData = excel.GetCellDataString(i, j);
-				System.out.print(cellData + " | ");
+				//System.out.print(cellData + " | ");
 				data[i-1][j] = cellData;
 
 			}
-			System.out.println();
+			//System.out.println();
 		}
 		return data;
-	}
+	}  
 
 }
--- src/test/java/util/ExcelUtilTest.java
+++ src/test/java/util/ExcelUtilTest.java
@@ -23,21 +23,21 @@ public class ExcelUtilTest {
 	public int getRowCount() {
 		int rowCount = 0;
 		rowCount = sheet.getPhysicalNumberOfRows();
-		System.out.println("No of Rows : " + rowCount);
+		//System.out.println("No of Rows : " + rowCount);
 		return rowCount;
 	}
 
 	public int getColCount() {
 		int colCount = 0;
 		colCount = sheet.getRow(0).getPhysicalNumberOfCells();
-		System.out.println("No of Columns : " + colCount);
+		//System.out.println("No of Columns : " + colCount);
 		return colCount;
 	}
 
 	public String GetCellDataString(int rowNum, int colNum) {
 		String cellData = null;
 		cellData = sheet.getRow(rowNum).getCell(colNum).getStringCellValue();
-		System.out.println(cellData);
+		//System.out.println(cellData);
 		return cellData;
 
 	}
@@ -45,7 +45,7 @@ public class ExcelUtilTest {
 	public double GetCellDataNumber(int rowNum, int colNum) {
 		double cellData = 0;
 		cellData = sheet.getRow(rowNum).getCell(colNum).getNumericCellValue();
-		System.out.println(cellData);
+		//System.out.println(cellData);
 		return cellData;
 
 	}
